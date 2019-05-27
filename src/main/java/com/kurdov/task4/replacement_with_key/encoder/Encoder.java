9d2d37bcb542cec7de36da05f0beebbe01c62f2a
Rustam Gurd <rustamkurdov@mail.ru>
2019-05-27 04:13:44 +0300

    Replacement with key algorithm decryption implemented

--- src/main/java/com/kurdov/task4/replacement_with_key/encoder/Encoder.java
+++ src/main/java/com/kurdov/task4/replacement_with_key/encoder/Encoder.java
@@ -6,28 +6,7 @@ import java.util.Comparator;
 public class Encoder {
 
     public String encrypt(String data, String key) {
-
-        int dataLength = data.length();
-        int keyWidth = key.length();
-
-        int tableDepth = dataLength / keyWidth;
-        //1 for key-header and 1 more if extra letters left
-        tableDepth = (dataLength % keyWidth != 0) ? tableDepth + 2 : tableDepth + 1;
-
-        char[][] table = new char[keyWidth][tableDepth];
-        int dataCursor = 0;
-        int keyCursor = 0;
-        for (int columnCursor = 0; columnCursor < tableDepth; columnCursor++) {
-            for (int lineCursor = 0; lineCursor < keyWidth; lineCursor++) {
-                if (columnCursor == 0) {
-                    table[lineCursor][columnCursor] = key.charAt(keyCursor++);
-                } else {
-                    if (dataCursor < data.length()) {
-                        table[lineCursor][columnCursor] = data.charAt(dataCursor++);
-                    }
-                }
-            }
-        }
+        char[][] table = fillEncryptionTable(data, key);
 
         Arrays.sort(table, Comparator.comparingInt(o -> o[0]));
 
@@ -42,10 +21,104 @@ public class Encoder {
         return encryptedData.toString();
     }
 
+    private char[][] fillEncryptionTable(String data, String key) {
+        int dataLength = data.length();
+        int keyWidth = key.length();
+
+        int tableDepth = dataLength / keyWidth;
+        //1 for key-header and 1 more if extra letters left
+        tableDepth = (dataLength % keyWidth != 0) ? tableDepth + 2 : tableDepth + 1;
+
+        char[][] table = new char[keyWidth][tableDepth];
+        int dataCursor = 0;
+        int keyCursor = 0;
+
+        for (int lineCursor = 0; lineCursor < keyWidth; lineCursor++) {
+            table[lineCursor][0] = key.charAt(keyCursor++);
+        }
+
+        for (int columnCursor = 1; columnCursor < tableDepth; columnCursor++) {
+            for (int lineCursor = 0; lineCursor < keyWidth; lineCursor++) {
+                if (dataCursor < data.length()) {
+                    table[lineCursor][columnCursor] = data.charAt(dataCursor++);
+                }
+            }
+        }
+
+        return table;
+    }
+
+    public String decrypt(String data, String key) {
+
+        char[] chars = key.toCharArray();
+        Arrays.sort(chars);
+        String sortedKey = new String(chars);
+        char[][] table = fillDecryptionTable(data, sortedKey);
+
+        //Sorting table back
+        char[][] decryptedTable = new char[table.length][table[0].length];
+        for (int i = 0; i < key.length(); i++) {
+            char c = key.charAt(i);
+            for (int j = 0; j < table.length; j++) {
+                char[] column = table[j];
+                if (column[0] == c) {
+                    decryptedTable[i] = column;
+                    table[j] = new char[1];
+                    break;
+                }
+            }
+        }
+
+        StringBuilder decryptedData = new StringBuilder();
+
+        int dataCursor = 0;
+        for (int columnCursor = 1; columnCursor < decryptedTable[0].length; columnCursor++) {
+            for (int lineCursor = 0; lineCursor < key.length(); lineCursor++) {
+                if (dataCursor < data.length()) {
+                    decryptedData.append(decryptedTable[lineCursor][columnCursor]);
+                    dataCursor++;
+                }
+            }
+        }
+
+        return decryptedData.toString();
+
+    }
+
+    private char[][] fillDecryptionTable(String data, String key) {
+        int dataLength = data.length();
+        int keyWidth = key.length();
+
+        int tableDepth = dataLength / keyWidth;
+        //1 for key-header and 1 more if extra letters left
+        tableDepth = (dataLength % keyWidth != 0) ? tableDepth + 2 : tableDepth + 1;
+
+        char[][] table = new char[keyWidth][tableDepth];
+        int dataCursor = 0;
+        int keyCursor = 0;
+
+        for (int lineCursor = 0; lineCursor < keyWidth; lineCursor++) {
+            table[lineCursor][0] = key.charAt(keyCursor++);
+        }
+
+            for (int lineCursor = 0; lineCursor < keyWidth; lineCursor++) {
+                for (int columnCursor = 1; columnCursor < tableDepth; columnCursor++) {
+                    if (dataCursor < data.length()) {
+                    table[lineCursor][columnCursor] = data.charAt(dataCursor++);
+                }
+            }
+        }
+
+        return table;
+    }
+
     public static void main(String[] args) {
         Encoder encoder = new Encoder();
-        String s = encoder.encrypt("перестановочный шифр", "шифр");
 
-        System.out.println(s);
+        String encr = encoder.encrypt("перестановочный шифр", "шифр");
+        System.out.println(encr);
+
+        String decr = encoder.decrypt(encr, "шифр");
+        System.out.println(decr);
     }
 }
